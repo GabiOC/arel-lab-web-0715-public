6cba44b4e905150c683d2fc5ca8afbb7149a9a34
GabiOC <oconnor.gabriele@gmail.com>
2015-09-04 14:40:31 -0400

    all done

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,36 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    limit(5)
+  end
+
+  def self.dinghy
+    where('length < 20')
+  end
+
+  def self.ship
+    where('length > 20')
+  end
+
+  def self.last_three_alphabetically
+    order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+    where(captain: nil)
+  end
+
+  def self.sailboats
+    sailboat_id = Classification.where(name: 'Sailboat').pluck(:id)
+    sailboat = BoatClassification.where(classification_id: sailboat_id).pluck(:boat_id)
+    self.where(id: sailboat)
+  end
+
+  def self.with_three_classifications
+    boats = BoatClassification.all.pluck(:boat_id)
+    three_classes = boats.select{|boat| boats.count(boat) == 3}.uniq
+    self.where(id: three_classes)
+  end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,38 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    catamaran_id = Classification.where(name: 'Catamaran').pluck(:id)
+    catamaran = BoatClassification.where(classification_id: catamaran_id).pluck(:boat_id)
+    captain_id = Boat.where(id: catamaran).pluck(:captain_id)
+    self.where(id: captain_id)
+  end
+
+  def self.sailors
+    captain_id = Boat.sailboats.pluck(:captain_id)
+    self.where(id: captain_id)
+  end
+
+  def self.talented_seamen
+    # find captains of sailboats
+    # find captains of motorboats
+    # find overlap
+
+    motorboat_id = Classification.where(name: 'Motorboat').pluck(:id)
+    motorboat = BoatClassification.where(classification_id: motorboat_id).pluck(:boat_id)
+    captain_id = Boat.where(id: motorboat).pluck(:captain_id)
+    motorboat_captains = self.where(id: captain_id)
+
+    sailboat_captains = self.sailors
+
+    overlap = motorboat_captains & sailboat_captains
+    # convoluted workaround below to turn array into ActiveRecord::Relation for 'pluck' method in spec
+    self.where(id: overlap.map(&:id))
+  end
+
+  def self.non_sailors
+    sailors = self.sailors
+    where.not(id: sailors)
+  end
+
 end
